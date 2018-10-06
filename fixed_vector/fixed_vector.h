bc141250d4013149168d7d0f3d147dacb4f6797f
Donat Sokolov <32036193+damm1t@users.noreply.github.com>
2018-10-06 19:47:35 +0300

    upd fixed_vector

--- fixed_vector/fixed_vector.h
+++ fixed_vector/fixed_vector.h
@@ -6,7 +6,6 @@
 #define FIXED_VECTOR_VECTOR_H
 
 #include <cstddef>
-#include <type_traits>
 #include <iterator>
 #include <cassert>
 
@@ -14,7 +13,7 @@ template<typename T, size_t CAPACITY>
 struct fixed_vector {
 private:
     size_t size_;
-    T *data_ = static_cast<T*>(operator new(CAPACITY * sizeof(T)));
+    typename std::aligned_storage<sizeof(T), alignof(T)>::type data_[CAPACITY];
 
 public:
 
@@ -22,7 +21,12 @@ public:
 
     fixed_vector(fixed_vector const &other) : size_(other.size_) {
         for (size_t i = 0; i < size_; i++)
-            new(data_ + i)T(*(other.data_ + i));
+            new(data_ + i)T(*reinterpret_cast<const T *>(other.data_ + i));
+    }
+
+    ~fixed_vector(){
+        for (size_t i = 0; i < size_; i++)
+            reinterpret_cast<T*>(data_ + i)->~T();
     }
 
     fixed_vector &operator=(fixed_vector other) {
@@ -37,14 +41,14 @@ public:
 
     void pop_back() {
         assert(0 < size());
-        (data_ + --size_)->~T();
+        reinterpret_cast<T *>(data_ + --size_)->~T();
     }
 
     void push_front(T const &value) {
         assert(size() < CAPACITY);
         for (size_t i = size_; i > 0; i--) {
-            new(data_ + i)T(*(data_ + i - 1));
-            (data_ + i - 1)->~T();
+            new(data_ + i)T(*reinterpret_cast<T *>(*(data_ + i - 1)));
+            reinterpret_cast<T *>(data_ + i - 1)->~T();
         }
         new(data_)T(value);
         size_++;
@@ -55,8 +59,8 @@ public:
         (data_)->~T();
         size_--;
         for (size_t i = 0; i < size_; i++) {
-            new(data_ + i)T(*(data_ + i + 1));
-            (data_ + i + 1)->~T();
+            new(data_ + i)T(*reinterpret_cast<T *>(*(data_ + i + 1)));
+            reinterpret_cast<T *>(data_ + i + 1)->~T();
         }
     }
 
@@ -74,12 +78,12 @@ public:
 
     T operator[](size_t ind) const {
         assert(0 <= ind && ind < size_);
-        return *(data_ + ind);
+        return *reinterpret_cast<T *>(data_ + ind);
     }
 
     T &operator[](size_t ind) {
         assert(0 <= ind && ind < size_);
-        return *(data_ + ind);
+        return *reinterpret_cast<T *>(data_ + ind);
     }
 
     friend void swap(fixed_vector<T, CAPACITY> &_one, fixed_vector<T, CAPACITY> &_two) {
@@ -87,17 +91,17 @@ public:
         std::swap(_one.data_, _two.data_);
     }
 
-    using iterator = T*;
-    using const_iterator = const T*;
+    using iterator = T *;
+    using const_iterator = const T *;
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
     iterator begin() {
-        return data_;
+        return reinterpret_cast<T *>(data_);
     }
 
     iterator end() {
-        return data_ + size_;
+        return reinterpret_cast<T *>(data_ + size_);
     }
 
     reverse_iterator rbegin() {
@@ -109,11 +113,11 @@ public:
     }
 
     const_iterator begin() const {
-        return const_iterator(data_);
+        return reinterpret_cast<const T *>(data_);
     }
 
     const_iterator end() const {
-        return const_iterator(const_cast<const T *>(data_ + size_));
+        return reinterpret_cast<const T *>(data_ + size_);
     }
 
     const_reverse_iterator rbegin() const {
@@ -128,8 +132,8 @@ public:
         assert(size() < CAPACITY);
         assert(begin() <= index && index <= end());
         for (size_t i = size_; i > index - begin(); i--) {
-            new(data_ + i)T(*(data_ + i - 1));
-            (data_ + i - 1)->~T();
+            new(data_ + i)T(*reinterpret_cast<T *>(data_ + i - 1));
+            reinterpret_cast<T *>(data_ + i - 1)->~T();
         }
         new(data_ + (index - begin()))T(value);
         size_++;
@@ -138,23 +142,23 @@ public:
 
     iterator erase(const_iterator index) {
         assert(begin() <= index && index < end());
-        (data_ + (index - begin()))->~T();
+        reinterpret_cast<T *>(data_ + (index - begin()))->~T();
         --size_;
         for (size_t i = index - begin(); i < size_; i++) {
-            new(data_ + i)T(*(data_ + i + 1));
-            (data_ + i + 1)->~T();
+            new(data_ + i)T(*reinterpret_cast<T *>(data_ + i + 1));
+            reinterpret_cast<T *>(data_ + i + 1)->~T();
         }
-        return data_ + (index - data_);
+        return begin() + (index - begin());
     }
 
     const_iterator erase(const_iterator begin, const_iterator end) {
         assert(begin() <= begin && begin < end && end < end());
         for (auto i = begin; i != end; ++i) {
-            (data_ + (i - begin()))->~T();
+            reinterpret_cast<T *>(data_ + (i - begin()))->~T();
             --size_;
         }
         for (auto i = end, j = begin; i != end(); i++, j++) {
-            new(data_ + j)T(*(data_ + i));
+            new(data_ + j)T(* reinterpret_cast<T *>(data_ + i));
             (data_ + i)->~T();
         }
         return begin;
@@ -162,12 +166,12 @@ public:
 
     T front() {
         assert(size_ > 0);
-        return *(data_);
+        return *reinterpret_cast<T *>(data_);
     }
 
     T back() {
         assert(size_ > 0);
-        return *(data_ + size_ - 1);
+        return *reinterpret_cast<T *>(data_ + size_ - 1);
     }
 
     void clear() {
